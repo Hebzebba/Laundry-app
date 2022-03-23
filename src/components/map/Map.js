5be1454f8c34e75ee7d1c6c8686961214a675168
Hebzebba <kwashieseth34@gmail.com>
2022-03-23 17:16:21 +0000

    fix map feature

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,24 +1,30 @@
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useState } from "react";
-import Map from "react-map-gl";
-
+import { useState, useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "./Map.css";
 const MapBox = () => {
-  const [viewset, setVeiwSet] = useState({
-    latitude: 6.083333,
-    longitude: -0.25,
-    zoom: 9,
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
   });
 
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoic2V0aGdyZWdvcnkiLCJhIjoiY2wweHZ0d29uMGp0ZzNtc2R5bGFidHVoeCJ9.YT86gZa8hPmJ_XaC44qpNQ";
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
   return (
-    <>
-      <Map
-        initialViewState={{ ...viewset }}
-        style={{ width: "100%", height: "100%" }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken="pk.eyJ1Ijoic2V0aGdyZWdvcnkiLCJhIjoiY2wweHZ0d29uMGp0ZzNtc2R5bGFidHVoeCJ9.YT86gZa8hPmJ_XaC44qpNQ"
-      />
-    </>
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
   );
 };
-
 export default MapBox;
